55be9e04e2ba59b4b869412d0046b134aadeea58
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-07 15:23:01 +0100

    Add files via upload

---
+++ Chapter07/7.51.cpp
@@ -0,0 +1,7 @@
+// Exercise 7.51:
+/*
+ Why do you think vector defines its single-argument constructor as explicit, but string does not?
+ 
+ According to jaege:
+ ...because a number is different to a vector, whereas a string is just an array of characters.
+ */
