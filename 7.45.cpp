131a0d377e8355ab554c6697a12ccf4c56ce3a38
ithelpdec <34002836+ithelpdec@users.noreply.github.com>
2022-09-07 13:49:20 +0100

    Delete 7.45.cpp

--- 7.45.cpp
+++
@@ -1,9 +0,0 @@
-// Exercise 7.45
-/*
- What if we defined the vector in the previous exercise to hold objects of type C?
- 
- If vector defined in the previous exercise to hold objects of type C then the following operation would be legal
- 
- vector<C> vec(10);
- */
-
