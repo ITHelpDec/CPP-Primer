feeb09c49c38b8d3b6093a968e800629f0bc1f6e
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-07 22:20:26 +0100

    Add files via upload

---
+++ Chapter07/7.56.cpp
@@ -0,0 +1,14 @@
+// Exercise 7.56:
+/*
+ What is a static class member?
+ >> A static class member is a member associated with the class, rather than the individual objects of the class type
+ 
+ What are the advantages of static members?
+ >> It's efficient (no reason for each object to store a copy of the member – it's constant throughout)
+ >> When the static member value changes, it is reflected in the calculations of other members
+ >> A static data member can have incomplete type
+ >> Can be used as a default argument
+ 
+ How do they differ from ordinary members?
+ >> A static member belongs to the class, whereas a nonstatic member belongs to the objects of the class
+*/
