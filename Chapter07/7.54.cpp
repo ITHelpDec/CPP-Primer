665d0658cfc7a1a6fd27f543bfab6b41071bb757
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-07 16:41:34 +0100

    Add files via upload

---
+++ Chapter07/7.54.cpp
@@ -0,0 +1,7 @@
+// Exercise 7.54:
+/*
+ Should the members of Debug that begin with set_ be declared as constexpr?
+ If not, why not?
+ 
+ Members of Debug beginning with set_ should NOT be declared as constexpr as they do not contain a return statement
+ */
