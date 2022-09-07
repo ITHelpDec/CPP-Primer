43631ecf293646e31205fe66bbc9d39fae843e48
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-07 16:46:05 +0100

    Add files via upload

---
+++ Chapter07/7.55.cpp
@@ -0,0 +1,14 @@
+// Exercise 7.55:
+/*
+ Is the Data class from § 7.5.5 (p. 298) a literal class?
+ If not, why not?
+ If so, explain why it is literal.
+ 
+ The code below is not a literal class because it doesn't return literal values (they are not initialised)
+ 
+*/
+
+struct Data {
+    int ival;
+    string s;
+};
