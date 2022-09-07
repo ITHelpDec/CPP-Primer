fded6c0ddbc5087c505d9e8c6fe255303cadc034
ithelpdec <34002836+ithelpdec@users.noreply.github.com>
2022-09-07 12:56:27 +0100

    Add files via upload

---
+++ 7.44.cpp
@@ -0,0 +1,10 @@
+// Exercise 7.44
+/*
+ Is the following declaration legal?
+ If not, why not?
+ 
+ vector<NoDefault> vec(10);
+ 
+ Illegal – no constructor to call for NoDefault, won't compile
+ */
+
