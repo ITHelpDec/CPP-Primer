39d8326f55cf1a22a69dbf6c8d55d16e75671e1f
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-09 19:12:31 +0100

    Add files via upload

---
+++ Chapter09/9.02.cpp
@@ -0,0 +1,13 @@
+// Exercise 9.02:
+/*
+ Define a list that holds elements that are deques that hold ints.
+*/
+
+#include <list>
+#include <deque>
+int main()
+{
+    std::list<std::deque<int>> ldq;
+    
+    return 0;
+}
