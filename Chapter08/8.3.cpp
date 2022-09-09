985acbd5a68bb3b1f6b22db22e7880675849655b
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-09 10:39:58 +0100

    Delete Chapter08 directory

--- Chapter08/8.3.cpp
+++
@@ -1,9 +0,0 @@
-// Exercise 8.3:
-/*
- What causes the following while to terminate? while (cin >> i) ...
- 
- The following will cause a while loop to terminate:
- >> Reaching end-of-file (eofbit and failbit)
- >> A recoverable error e.g. reading a character when numeric data was expected (failbit)
- >> A system-level failure e.g. an unrecoverable read of write (badbit)
-*/
