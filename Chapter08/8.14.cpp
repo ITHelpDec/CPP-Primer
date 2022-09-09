6a3fc57823280a7852e2a0fba3b27c7ca778d527
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-09 17:06:06 +0100

    Add files via upload

---
+++ Chapter08/8.14.cpp
@@ -0,0 +1,7 @@
+// Exercise 8.14:
+/*
+ Why did we declare entry and nums as const auto &?
+ 
+ >> By usng "&" we refer to the source instead of needlessly making copies of each element
+ >> By using "const" we protect ourselves from accidentally changing the values we're referring to
+*/
