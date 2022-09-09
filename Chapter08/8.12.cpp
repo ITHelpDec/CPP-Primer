ae890fdad9703643398b699f8107377a54925b2a
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-09 13:36:58 +0100

    Add files via upload

---
+++ Chapter08/8.12.cpp
@@ -0,0 +1,7 @@
+// Exercise 8.12:
+/*
+ Why didn’t we use in-class initializers in PersonInfo?
+ 
+ >> No need – a default constrcutor will do the necessary when PersonInfo is created
+ >> string and vector also have default constructors
+ */
