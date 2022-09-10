4bf14d90f320254b8667f33c8b7cb1aa89a8c70b
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-10 20:23:45 +0100

    Add files via upload

---
+++ Chapter09/9.09.cpp
@@ -0,0 +1,8 @@
+// Exercise 9.09:
+/*
+ What is the difference between the begin and cbegin functions?
+ 
+ >> begin() is ::iterator (read and write, ascending)
+ >> cbegin() is ::const_iterator (only read, ascending)
+*/
+
