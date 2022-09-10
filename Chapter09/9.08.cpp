96fe7e80c0e8d28f0ddc0030eb2cee8e211f49da
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-10 20:15:02 +0100

    Add files via upload

---
+++ Chapter09/9.08.cpp
@@ -0,0 +1,9 @@
+// Exercise 9.08:
+/*
+ What type should be used to read elements in a list of strings? To write them?
+ 
+ >> std::list<string>::const_iterator or std::list<string>::iterator can be used to read,
+ >> but only std::list<string>::iterator can be used to write
+ >> remember: const protects you from over-writing by mistake
+*/
+
