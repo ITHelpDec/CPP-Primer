ace7218598eaab4965e07c63430d8ef6bc1802c5
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-12 11:35:48 +0100

    Add files via upload

---
+++ Chapter09/9.30.cpp
@@ -0,0 +1,8 @@
+// Exercise 9.30:
+/*
+ What, if any, restrictions does using the version of resize that takes a
+ single argument place on the element type?
+ 
+ >> .resize() cannot be used on arrays
+ >> "If the container holds elements of a class type and resize adds elements, we must supply an initializer or the element type must have a default constructor." pg 352
+ */
