130e47b91b321058e9e749821602925ac4e5dca1
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-12 15:51:04 +0100

    Add files via upload

---
+++ Chapter09/9.40.cpp
@@ -0,0 +1,10 @@
+// Exercise 9.40:
+/*
+ If the program in the previous exercise reads 256 words, what is its likely capacity after it is resized? What if it reads 512? 1,000? 1,048?
+ 
+ >> 256 words: 1024 (no change)
+ >> 512 words: 1024 (no change)
+ >> 1000 words: 1024 (no change)
+ >> 1048 words: 1048 (capacity increaes to accommodate increased demand)
+ 
+*/
