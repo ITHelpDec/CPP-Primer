9f3975bca798bf23776a6d6991f2d8d1bbfc268c
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-12 15:15:41 +0100

    Add files via upload

---
+++ Chapter09/9.35.cpp
@@ -0,0 +1,7 @@
+// Exercise 9.35:
+/*
+ Explain the difference between a vector’s capacity and its size.
+
+ >> capacity is the memory allocated to the container i.e. how many elements the container CAN hold
+ >> size is how many elements are in the container i.e. how many elements the container IS holding
+*/
