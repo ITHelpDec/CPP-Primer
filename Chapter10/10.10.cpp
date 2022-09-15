ce00b6a328d396dcc05b68c07e0968432c9ba1dc
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-15 19:41:00 +0100

    Add files via upload

---
+++ Chapter10/10.10.cpp
@@ -0,0 +1,7 @@
+// Exercise 10.10:
+/*
+ Why do you think the algorithms don’t change the size of containers?
+ 
+ >> Because the role of an algorithm is to read, write or copy
+ >> If we want to change the size of a container, then we call the member functions specific to that container
+*/
