060a46cdecd55b6b87516354c7c84f122e9747b3
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-17 21:10:18 +0100

    Add files via upload

---
+++ Chapter10/10.23.cpp
@@ -0,0 +1,6 @@
+// Exercise 10.23:
+/*
+ How many arguments does bind take?
+ 
+ >> bind takes as many arguments as the function it's impersonating (the callable), plus one (the name of the callable function itself, placed at the beginning of the bind)
+*/
