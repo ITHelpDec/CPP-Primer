74c959f9eafd79f1e1bc534c40c31e86e907d782
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-18 09:59:54 +0100

    Add files via upload

---
+++ Chapter10/10.26.cpp
@@ -0,0 +1,15 @@
+// Exercise 10.26:
+/*
+ Explain the differences among the three kinds of insert iterators.
+ 
+ >> back_inserter()
+ >> creates an iterator that uses push_back()
+ 
+ >> front_inserter()
+ >> creates an iterator that uses push_front()
+ 
+ >> inserter()
+ >> creates an iterator that uses insert()
+ >> This fucntion takes a second argument, which must be an iterator into the given container.
+ >> Elements are inserted ahead of the elment denoted by the given iterator
+*/
