122ec1c3c92a259c0025fca239835ade5cfc8813
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-19 22:38:02 +0100

    Add files via upload

---
+++ Chapter10/10.40.cpp
@@ -0,0 +1,11 @@
+// Exercise 10.40:
+/*
+ What kinds of iterators do you think copy requires? What about reverse or unique?
+ 
+ >> std::copy -> arguments _1 and _2 are intput operators; argument 3 is an output operator
+ 
+ >> reverse -> requires bi-directional iterators
+ 
+ >> unique -> requires forward iterators
+*/
+
