c4f73eaa3189087fb66a0ec65f1671dc9d5d2d04
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-20 16:46:31 +0100

    Add files via upload

---
+++ Chapter11-Associative_Containers/11.01.cpp
@@ -0,0 +1,13 @@
+// Exercise 11.01:
+/*
+ Describe the differences between a map and a vector.
+ 
+ >> .:. MAP .:.
+ >> a map is an associative container that contains an associative array of "key <-> value" pairs.
+ >> values are found through their key, and the key does not have to be an integer
+ 
+ >> .:. VECTOR .:.
+ >> a vector is a sequential container that contains a standard array of values
+ >> values are found by position / index (these positions are integers)
+ 
+*/
