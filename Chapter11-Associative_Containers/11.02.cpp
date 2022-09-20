f1e712d83d96ff8b0f52c08af69c1efdf8e32615
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-20 16:56:19 +0100

    Add files via upload

---
+++ Chapter11-Associative_Containers/11.02.cpp
@@ -0,0 +1,21 @@
+// Exercise 11.02:
+/*
+ Give an example of when each of list, vector, deque, map, and set
+ might be most useful.
+ 
+ >> .:. LIST .:.
+ >> Lists are helpful when you need quick insertion and deletion in the middle of the container, like in a linked list
+ 
+ >> .:. VECTOR .:.
+ >> A vector is useful is you need random access and quick insertion and deletion at the end of the container
+ 
+ >> .:. DEQUE .:.
+ >> Useful when working with queues (First-In-First-Out)
+ 
+ >> .:. MAP .:.
+ >> Maps are useful when you need associative arrays e.g. a dictinary where the word is the key, and its meaning is the value
+ 
+ >> .:. SET .:.
+ >> Sets might prove useful for checking where a value is present e.g. exclusion lists
+ 
+*/
