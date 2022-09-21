0619c951b33262d28c088090450b8c83bb8ca181
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-21 12:01:44 +0100

    Add files via upload

---
+++ Chapter11-Associative_Containers/11.06.cpp
@@ -0,0 +1,14 @@
+// Exercise 11.06:
+/*
+ Explain the difference between a set and a list.
+ When might you use one or the other?
+ 
+ A set is an associative container, whereas a list is a sequential container
+ 
+ Elements in a list can be manually sorted by the programmer, whereas elements in a set are sorted by default (unless it's an unsorted version)
+ 
+ A list is ordered by default, whereas a set is un-ordered.
+ 
+ We can't enforce an arbitrary order by placing an arbitrary element in an arbitrary position in a set.
+ 
+*/
