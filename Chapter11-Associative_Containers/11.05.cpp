608440a9214fba71f95d177f5c51d98652205aab
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-21 11:47:47 +0100

    Add files via upload

---
+++ Chapter11-Associative_Containers/11.05.cpp
@@ -0,0 +1,14 @@
+// Exercise 11.05:
+/*
+ Explain the difference between a map and a set.
+ When might you use one or the other?
+ 
+ .:. MAP .:.
+ >> A map is an associative array of a "key <-> value"
+ >> It would be useful for something like a dictionary
+ 
+ .:. SET .:.
+ >> A set is collection of keys
+ >> It would be useful for an inclusion or an exclusion list
+ 
+*/
