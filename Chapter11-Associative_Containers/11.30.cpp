c3b274911438d2967caf552436469a4dc5422425
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-24 17:37:07 +0100

    Add files via upload

---
+++ Chapter11-Associative_Containers/11.30.cpp
@@ -0,0 +1,6 @@
+// Exercise 11.30:
+/*
+ Explain the meaning of the operand pos.first->second used in the output expression of the final program in this section.
+ 
+ >> "pos.first->second" dereferences the position stored in ::iterator "pos.first" to fetch the value element of the mapped key <-> value pair
+ */
