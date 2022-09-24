3f0ac75c3806b4c71d68fc9ecf8061bea47a8eb2
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-24 15:12:40 +0100

    Add files via upload

---
+++ Chapter11-Associative_Containers/11.27.cpp
@@ -0,0 +1,7 @@
+// Exercise 11.27:
+/*
+ What kinds of problems would you use count to solve? When might you use find instead?
+ 
+ >> I would use .count() for the likes of multimaps where we would want to know the number of instances a given key appears
+ >> I would use find when working with associative containers that do not have multiple records of the same key e.g. is the key present?
+ */
