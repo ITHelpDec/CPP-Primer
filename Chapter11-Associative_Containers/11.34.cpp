2d0d0fc40edc44bdc616baeff714f4b5c7895ec1
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-25 10:50:27 +0100

    Add files via upload

---
+++ Chapter11-Associative_Containers/11.34.cpp
@@ -0,0 +1,6 @@
+// Exercise 11.34:
+/*
+ What would happen if we used the subscript operator instead of find in the transform function?
+ 
+ >> If we were to use to subscript operator, then should the key not exist already, then it will be created and inserted into the map with an empty associative value
+*/
