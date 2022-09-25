2883e1f83850f40fac5cd7696364d7e5298bcb42
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-25 19:08:09 +0100

    Add files via upload

---
+++ Chapter11-Associative_Containers/11.37.cpp
@@ -0,0 +1,10 @@
+// Exercise 11.37:
+/*
+ What are the advantages of an unordered container as compared to the ordered version of that container?
+ What are the advantages of the ordered version?
+ 
+ >> "An unordered container is most useful when we have a key type for which there is no obvious ordering relationship among the elements. These containers are also useful for applications in which the cost of maintaining the elements in order is prohibitive." - pg 443
+ 
+ >> "Although hashing gives better average case performance in principle, achieving good results in practice often requires a fair bit of performance testing and tweaking. As a result, it is usually easier (and ofter yields better performance) to use an ordered container." - pg 443
+ >>
+*/
