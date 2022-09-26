601bd856cbb6b22d58bd830f54168ff03360a89a
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-26 13:58:33 +0100

    Add files via upload

---
+++ Chapter12-Dynamic Memory/12.03.cpp
@@ -0,0 +1,8 @@
+// Exercise 12.03:
+/*
+ Does this class need const versions of push_back and pop_back?
+ If so, add them.
+ If not, why aren’t they needed?
+ 
+ >> I would not add const or  const-qualified versions of push_back or pop_back as they need to remain non-const to modify the contents of objects
+*/
