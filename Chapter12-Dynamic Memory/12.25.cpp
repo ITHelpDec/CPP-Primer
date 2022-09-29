5d794192897d2979ec61e8a0d8883c1e7305c693
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-29 16:40:20 +0100

    Add files via upload

---
+++ Chapter12-Dynamic Memory/12.25.cpp
@@ -0,0 +1,8 @@
+// Exercise 12.25:
+/*
+ Given the following new expression, how would you delete pa?
+ // int *pa = new int[10];
+ 
+ I would use the following code to delete pa
+ // delete [] pa;
+*/
