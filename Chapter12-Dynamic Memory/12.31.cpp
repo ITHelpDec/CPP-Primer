a6d9ee6532e0dfa3332f362d595763378389baa6
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-09-30 19:46:43 +0100

    Add files via upload

---
+++ Chapter12-Dynamic Memory/12.31.cpp
@@ -0,0 +1,8 @@
+// Exercise 12.31:
+/*
+ What difference(s) would it make if we used a vector instead of a set to hold the line numbers? Which approach is better?
+ Why?
+ 
+ >> The vector would record instances where the query occured more than once in one line, which might be beneficial for overall occurences, rather than recording instances of line appearances like a set would do.
+ >> Both have unique benefits, but it all depends on the desired outocome of the programme.
+*/
