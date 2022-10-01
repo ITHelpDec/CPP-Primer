3b86121b1acdbfaa130eb4eefae0583eab12ebeb
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-10-01 15:12:32 +0100

    Add files via upload

---
+++ Chapter 13 - Copy Control/13.01.cpp
@@ -0,0 +1,11 @@
+// Exercise 13.01:
+/*
+ ""The copy and move constructors define what happens when an object in initialised from another object of the same type." - pg. 496
+ 
+ "A constructor is copy constructor if its first parameter is a reference to the class type and any additional parameters have default values." - pg. 496
+ 
+ >> "Copy initialisation happens not only when we define variables using an =, but also when we..."
+ >> "• Pass an object as an argument  to a parameter of nonreference type"
+ >> "• Return an object from a function that has a nonreference return type"
+ >> "• Brace initialise the elements in an array or the members of an aggregate class"
+*/
