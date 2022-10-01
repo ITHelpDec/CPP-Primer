950790dfec8d97988420bf49a73a93c46293bc42
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-10-01 18:31:51 +0100

    Add files via upload

---
+++ Chapter 13 - Copy Control/13.06.cpp
@@ -0,0 +1,9 @@
+// Exercise 13.06:
+/*
+ What is a copy-assignment operator? When is this operator used?
+ What does the synthesized copy-assignment operator do? When is it synthesized?
+ 
+ >> "A copy-assignment operator (operator=) assigns each nonstatic memeber of the right-hand object to the corresponding member of the left-hand object using the copy-assignment operator for the type of that member...the [synthesised] copy-assignement operator returns a reference to its left-hand object." – pg. 500
+ 
+ "..., the compiler synthesises a copy-assignment operator if the class does not define its own." – pg. 500
+*/
