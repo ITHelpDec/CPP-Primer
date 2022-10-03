d43e65f8f332766232d11afbfefe522b6ed17913
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-10-03 12:22:06 +0100

    Add files via upload

---
+++ Chapter 13 - Copy Control/13.21.cpp
@@ -0,0 +1,9 @@
+// Exercise 13.21:
+/*
+ Do you think the TextQuery and QueryResult classes need to define their own versions of the copy-control members?
+ If so, why?
+ If not, why not?
+ Implement whichever copy-control operations you think these classes require.
+ 
+ >> You could declare the copy-control members for the TextQuery and QueryResult class, but there's no need – the synthesised versions will do the job just fine.
+*/
