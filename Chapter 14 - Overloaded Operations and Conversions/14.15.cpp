96a83dd32849fbce955d1c1e0fc31552f7df4856
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-10-13 17:12:16 +0100

    Add files via upload

---
+++ Chapter 14 - Overloaded Operations and Conversions/14.15.cpp
@@ -0,0 +1,9 @@
+// Exercise 14.15:
+/*
+ Should the class you chose for exercise 7.40 from § 7.5.1 (p. 291) define any of the arithmetic operators?
+ If so, implement them.
+ If not, explain why not.
+ 
+ >> I wouldn't, personally.
+ >> I don't feel there's a need to perform arithmetic work to colours, ignition states or gears - there are already functions to shift gears up and down and turn the car on and off
+*/
