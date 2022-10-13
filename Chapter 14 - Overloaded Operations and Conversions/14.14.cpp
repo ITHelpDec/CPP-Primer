7b2b04ebd0784d0947544164b7936f0b3f1676a7
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-10-13 17:05:58 +0100

    Add files via upload

---
+++ Chapter 14 - Overloaded Operations and Conversions/14.14.cpp
@@ -0,0 +1,8 @@
+// Exercise 14.14:
+/*
+ Why do you think it is more efficient to define operator+ to call operator+= rather than the other way around?
+ 
+ >> By doing it this way, as opposed to the other way round, we avoid creating an extra temporary object to complete the function
+ >> Keep overheads low.
+ 
+*/
