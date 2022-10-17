d46a92a2bc83f723a30a0ab5931bd9041288aaa7
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-10-18 00:50:59 +0100

    Add files via upload

---
+++ Chapter 14 - Overloaded Operations and Conversions/14.31.cpp
@@ -0,0 +1,7 @@
+// Exercise 14.31:
+/*
+ Our StrBlobPtr class does not define the copy constructor, assignment operator, or a destructor.
+ Why is that okay?
+ 
+ >> The compiler will do a good enough job with synthesised constructors and assignment operators due to the nature of the class' data members
+*/
