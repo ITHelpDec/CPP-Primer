597ead4425ed66970552faadcd9a7f3391ddcc1e
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-10-17 21:20:58 +0100

    Add files via upload

---
+++ Chapter 14 - Overloaded Operations and Conversions/14.29.cpp
@@ -0,0 +1,7 @@
+// Exercise 14.29:
+/*
+ We did not define a const version of the increment and decrement operators.
+ Why not?
+ 
+ >> If we were to define it as const, we would not be able to increment or decrement our objects.
+*/
