5bbbcd1f493b78199f690a5512f10647f442ada6
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-10-17 17:35:02 +0100

    Add files via upload

---
+++ Chapter 14 - Overloaded Operations and Conversions/14.27.StrBlobPtr.hpp
@@ -0,0 +1,43 @@
+
+#ifndef STRBLOBPTR_H
+#define STRBLOBPTR_H
+
+#include <string>
+#include <vector>
+#include <memory>
+
+class StrBlob;
+
+class StrBlobPtr {
+    friend class StrBlob;
+    friend std::ostream& printStrBlobPtr(std::ostream&, const StrBlobPtr&);
+    friend bool operator==(const StrBlobPtr&, const StrBlobPtr&);
+    friend bool operator!=(const StrBlobPtr&, const StrBlobPtr&);
+    friend bool operator<(const StrBlobPtr&, const StrBlobPtr&);
+    friend bool operator>(const StrBlobPtr&, const StrBlobPtr&);
+    friend bool operator<=(const StrBlobPtr&, const StrBlobPtr&);
+    friend bool operator>=(const StrBlobPtr&, const StrBlobPtr&);
+    
+public:
+    StrBlobPtr();
+    explicit StrBlobPtr(StrBlob&, std::size_t = 0);
+    
+    std::string& operator[](std::size_t &n);
+    const std::string& operator[](std::size_t &n) const;
+    
+    // "++" and "--" operators
+    StrBlobPtr& operator++();
+    StrBlobPtr& operator--();
+    StrBlobPtr& operator++(int);
+    StrBlobPtr& operator--(int);
+    
+    std::string& deref() const;
+    StrBlobPtr& incr();
+    
+private:
+    std::shared_ptr<std::vector<std::string>> check(std::size_t, const std::string&) const;
+    std::weak_ptr<std::vector<std::string>> wptr;
+    std::size_t curr;
+};
+
+#endif
