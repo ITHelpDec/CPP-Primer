fd4eb8f0e536de244b88d97ba79a9ddaf6c6f5ad
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-10-19 20:41:22 +0100

    Add files via upload

---
+++ Chapter 14 - Overloaded Operations and Conversions/14.41.cpp
@@ -0,0 +1,7 @@
+// Exercise 14.41:
+/*
+ Why do you suppose the new standard added lambdas?
+ Explain when you would use a lambda and when you would write a class instead.
+ 
+ >> It's both less work and looks much cleaner, especially when you only intend to use it once at most.
+*/
