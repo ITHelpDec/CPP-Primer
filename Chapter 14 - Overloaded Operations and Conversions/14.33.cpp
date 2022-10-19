509cee45fa58c1ce4ae04bd687e7ef749bbfd9d7
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-10-19 14:31:42 +0100

    Add files via upload

---
+++ Chapter 14 - Overloaded Operations and Conversions/14.33.cpp
@@ -0,0 +1,11 @@
+// Exercise 14.33:
+/*
+ How many operands may an overloaded function-call operator take?
+ 
+ >> "An overloaded oeprator function has the same number of parameters as the operator has operands. Hence, the maximum value should be 256".
+ 
+ >> This can be seen in "Annex B – implementation quantities" of the C++ Language
+ 
+ // "Parameters in one function definition [256]."
+ // "Arguments in one function call [256]."
+*/
