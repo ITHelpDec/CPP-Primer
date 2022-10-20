d168920696f6f24c1e982176bd1e20bb83e0be90
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-10-20 15:52:18 +0100

    Add files via upload

---
+++ Chapter 14 - Overloaded Operations and Conversions/14.46.cpp
@@ -0,0 +1,7 @@
+// Exercise 14.46:
+/*
+ Explain whether defining these Sales_data conversion operators is a good idea and whether they should be explicit.
+ 
+ >> Coversion operators in general is a bad idea.
+ >> If we need to define them then they should be explicit to avoid implicit conversion, but otherwise it's safer to define specific member functions
+*/
