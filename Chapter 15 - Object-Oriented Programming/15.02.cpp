3816ac20f6bd327b9c104849f915604ea79cab07
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-10-21 23:07:08 +0100

    Add files via upload

---
+++ Chapter 15 - Object-Oriented Programming/15.02.cpp
@@ -0,0 +1,7 @@
+// Exercise 15.02:
+/*
+ How does the protected access specifier differ from private?
+ 
+ // "However, sometimes a base class has members that it wants to let its derived classes use while still prohibiting access to those same members by other users. We specify such members after a protected access specifier."
+ // "Our Quote class expects its derived classes to define their own net_price function. To do so, those classes need access to the price member. As a result, Quote defines that member as protected." – pg. 595
+*/
