cda948069d903afc62b54c8340553b874ea6fac1
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-10-23 15:30:20 +0100

    Add files via upload

---
+++ Chapter 15 - Object-Oriented Programming/15.08.cpp
@@ -0,0 +1,10 @@
+// Exercise 15.08:
+/*
+ Define static type and dynamic type.
+ 
+ >> .:. STATIC TYPE .:.
+ // "The static type of an expression is always known at compile time – it is the type with which a variable is declared or that an expression yields." - pg. 601
+ 
+ >> .:. DYNAMIC TYPE .:.
+ "The dynamic type is the type of the object in memory that the variable or expression represents. The dynamic type may not be known until run time." – pg. 601
+*/
