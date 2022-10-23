90d93217b9a8120f6e2be1808f34d89e82d54a9a
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-10-23 23:43:05 +0100

    Add files via upload

---
+++ Chapter 15 - Object-Oriented Programming/15.12.cpp
@@ -0,0 +1,7 @@
+// Exercise 15.12:
+/*
+ Is it ever useful to declare a member function as both override and final?
+ Why or why not?
+ 
+ >> No – "final" is the same as "final override"; declaring both is an unecessary pleonasm.
+*/
