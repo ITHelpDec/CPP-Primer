9d1cf8cc63e8e492bf5c71e823edb8985a9ff91a
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-11-06 00:57:19 +0000

    Delete 15.33.cpp

--- 15.33.cpp
+++
@@ -1,7 +0,0 @@
-// Exercise 15.33:
-/*
- What about objects of type Query_base?
- 
- >> This will be managed by the default synthsised copy-control.
- >> N.B.: Query_base is set up as an "abstract base class", so we really only intend to derive other classes from this interface, rather than apply constructors or copy-control to it.
-*/
