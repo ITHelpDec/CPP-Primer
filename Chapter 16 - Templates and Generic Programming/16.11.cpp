ceac28d2f4eb9f331a0bf65305f33af560247036
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-12-03 20:49:12 +0000

    Add files via upload

---
+++ Chapter 16 - Templates and Generic Programming/16.11.cpp
@@ -0,0 +1,30 @@
+// Example 16.09:
+/*
+ The following definition of List is incorrect. How would you fix it?
+*/
+
+template <typename elemType> class ListItem;
+template <typename elemType> class List {
+public:
+    List<elemType>();
+    List<elemType>(const List<elemType> &);
+    List<elemType>& operator=(const List<elemType> &);
+    ~List();
+    void insert(ListItem *ptr, elemType value);
+private:
+    ListItem *front, *end;
+};
+
+// .insert() and private members "front" and "end" are missing template parameters
+
+template <typename elemType> class ListItem;
+template <typename elemType> class List {
+public:
+    List<elemType>();
+    List<elemType>(const List<elemType> &);
+    List<elemType>& operator=(const List<elemType> &);
+    ~List();
+    void insert(ListItem<elemType> *ptr, elemType value);
+private:
+    ListItem<elemType> *front, *end;
+};
