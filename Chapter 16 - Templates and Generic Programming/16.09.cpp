7087690d0d56984efaa5a9910e42135124dcc3f7
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-12-03 19:58:16 +0000

    Add files via upload

---
+++ Chapter 16 - Templates and Generic Programming/16.09.cpp
@@ -0,0 +1,10 @@
+// Example 16.09:
+/*
+ What is a function template?
+ 
+ >> A function template is a blueprint for generating functions. The compiler ordinarily deduces the template parameter type(s) using the arguments of the call.
+ 
+ What is a class template?
+ 
+ >> "A class template is a blueprint for generating classes. Class templates differ from function templates, in that the compiler cannot deduce the template parameter type(s) for a class template. Instead, as we've seen many times now, to use a class template we must supply additional information inside angle brackets following the template's name. That extra information is the list of template arguments to use in place of the template parameters." – pg 658
+*/
