8070766fe4f7ac56a5a244d8c95f33621ee34a95
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-12-09 18:27:40 +0000

    Add files via upload

---
+++ Chapter 16 - Templates and Generic Programming/16.23.cpp
@@ -0,0 +1,7 @@
+// Exercise 16.23:
+/*
+ Predict when the call operator will be executed in your main query programme.
+ If your expectations and what happens differ, be sure you understand why.
+ 
+ >> Once all instances of the shared_ptr have been destroyed, the DebugDelete destructor will be called.
+*/
