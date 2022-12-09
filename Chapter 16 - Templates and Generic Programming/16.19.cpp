bfce7ec7a48d69b30581cffb8171584b1d742d98
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-12-09 13:31:16 +0000

    Add files via upload

---
+++ Chapter 16 - Templates and Generic Programming/16.19.cpp
@@ -0,0 +1,32 @@
+// Exercise 16.19:
+/*
+ Write a function that takes a reference to a container and prints the elements in that container. Use the container’s size_type and size members to control the loop that prints the elements.
+ 
+ >> Did this in earlier exercise
+*/
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <unordered_set>
+#include <string>
+
+template <typename T> void printContainer(const T &c) {
+    for (const auto &e : c) {
+        std::cout << e << " ";
+    } std::cout << std::endl;
+}
+
+int main()
+{
+    std::vector<int> ivec = { 0, 1, 2, 3, 4 };
+    printContainer(ivec);
+    
+    std::list<char> clist = { 'a', 'b', 'c' };
+    printContainer(clist);
+    
+    std::unordered_set<std::string> usset = { "man", "bear", "pig" };
+    printContainer(usset);
+    
+    return 0;
+}
