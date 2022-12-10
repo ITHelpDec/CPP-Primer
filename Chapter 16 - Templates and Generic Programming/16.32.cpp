6f92a22722643085282ef284710d38213d16aee9
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-12-10 16:16:26 +0000

    Add files via upload

---
+++ Chapter 16 - Templates and Generic Programming/16.32.cpp
@@ -0,0 +1,10 @@
+// Exercise 16.32:
+/*
+ What happens during template argument deduction?
+ 
+ >> "We've seen that, by default, the compiler uses the argument in a call to determine the template parameters for a function template."
+ 
+ >> "The process of determining template arguments from the function arguments is known as TEMPLATE ARGUMENT DEDUCTION."
+ 
+ >> "During template argument deduction, the compiler uses types to the arguments in the call to find the template arguments that generate a version of the function that bet matches the given call." – pg. 678
+*/
