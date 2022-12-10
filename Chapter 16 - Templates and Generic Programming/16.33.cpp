049ab1c2a1508e2fa98ecbca172adc5fda4b51da
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-12-10 16:22:30 +0000

    Add files via upload

---
+++ Chapter 16 - Templates and Generic Programming/16.33.cpp
@@ -0,0 +1,10 @@
+// Exercise 16.33:
+/*
+ Name two type conversions allowed on function arguments involved
+ in template argument deduction.
+ 
+ >> (1) – "const" conversions
+ 
+ >> (2) – "array-to-pointer" or "function-to-pointer" conversions
+ 
+ */
