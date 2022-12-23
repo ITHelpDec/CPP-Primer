12448fd30bd60ac28019f08f1ebb507c8113868f
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-12-23 13:47:46 +0000

    Add files via upload

---
+++ Chapter 17 – Specialised Library Facilities/17.07.cpp
@@ -0,0 +1,22 @@
+// Exercise 17.07:
+/*
+ Explain which version of findBook you prefer and why.
+*/
+
+// I much prefer the use of another struct or class.
+
+// The ability to give more-easily-identifiable member names like...
+
+Matches().sz_
+Matches().cbegin_
+Matches().cend_
+
+// ...over...
+
+std::get<i>(std::tuple<Args>)
+
+// ...or...
+
+std::pair<Size, std::pair<It, It>>
+
+// ...make this an easy choice (for me, anyway)
