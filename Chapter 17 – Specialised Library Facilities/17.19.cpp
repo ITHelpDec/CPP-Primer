eeb941450831b908cdc2d789f0f6ec2487978713
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-12-24 22:33:13 +0000

    Add files via upload

---
+++ Chapter 17 – Specialised Library Facilities/17.19.cpp
@@ -0,0 +1,6 @@
+// Exercise 17.19:
+/*
+ Why is it okay to call m[4].str() without first checking whether m[4] was matched?
+ 
+ >> Since m[4] is optional ("?"), it will always return a result, regardless of whether or not it's matched (whether that be ".", "-", " " or "").
+*/
