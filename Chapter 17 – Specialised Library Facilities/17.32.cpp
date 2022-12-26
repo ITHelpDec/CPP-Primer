d6209fc61c6ecbfbb9b929bcdc27d47de3e6532b
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-12-26 02:03:36 +0000

    Add files via upload

---
+++ Chapter 17 – Specialised Library Facilities/17.32.cpp
@@ -0,0 +1,6 @@
+// Exercise 17.32:
+/*
+ What would happen if we defined resp inside the loop?
+ 
+ >> If "resp" were to be defined inside the loop, then it would only exist within the scope of the "do" loop, and would thus not be available for our "while" condition
+*/
