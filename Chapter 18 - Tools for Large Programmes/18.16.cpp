7b0acaa5b39f1f841369c06b1e34b587a50875b6
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2022-12-31 01:59:29 +0000

    Add files via upload

---
+++ Chapter 18 - Tools for Large Programmes/18.16.cpp
@@ -0,0 +1,10 @@
+// Exercise 18.16:
+/*
+ Explain the following code assuming using declarations for all the members of namespace Exercise are located at the location labeled position 1.
+ What if they appear at position 2 instead?
+ Now answer the same question but replace the using declarations with a using directive for namespace Exercise.
+ 
+ >> Please see 18.17 for answers and implementation.
+ >> https://github.com/ITHelpDec/CPP-Primer/tree/main/Chapter%2018%20-%20Tools%20for%20Large%20Programmes/18.17
+ 
+*/
