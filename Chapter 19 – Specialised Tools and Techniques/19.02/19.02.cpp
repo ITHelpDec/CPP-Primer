e8649d3335154f9c5421f9f6fd1356d87b3e90cd
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2023-01-04 14:54:03 +0000

    Update 19.02.cpp

--- Chapter 19 – Specialised Tools and Techniques/19.02/19.02.cpp
+++ Chapter 19 – Specialised Tools and Techniques/19.02/19.02.cpp
@@ -1,3 +1,9 @@
+// Exercise 19.02:
+/*
+ By default, the allocator class uses operator new to obtain storage and operator delete to free it.
+ Recompile and rerun your StrVec programs (§ 13.5, p. 526) using your versions of the functions from the previous exercise.
+*/
+
 #include "StrVec.hpp"
 
 int main()
