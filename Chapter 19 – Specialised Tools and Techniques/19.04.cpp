0be9a67458ae5637d675964d046a444e554b464e
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2023-01-04 18:20:34 +0000

    Add files via upload

---
+++ Chapter 19 – Specialised Tools and Techniques/19.04.cpp
@@ -0,0 +1,79 @@
+// Exercise 19.04:
+/*
+ Using the classes defined in the first exercise, ...
+ ...rewrite the following code to convert the expression *pa to the type C&:
+ 
+ if (C *pc = dynamic_cast< C* >(pa)) {
+ // use C’s members
+ } else {
+ // use A’s members
+ }
+ 
+ >> another not-so-great / vague question
+ >> a lot of these code samples seem to be supplied without context
+ >> RTTI doesn't seem to have the best impression (it's frowned on by Google, at least), ...
+ >> ...but a better understanding as to why would be more helpful
+ 
+ */
+
+#include <iostream>
+
+class A {
+public:
+    A() = default;
+    virtual ~A() { }
+};
+
+class B : virtual public A {
+public:
+    B() : A() { }
+    virtual ~B() { }
+};
+
+class C : public B {
+public:
+    C() : B() { }
+    virtual ~C() { }
+};
+
+class D : public B, virtual public A {
+public:
+    D() : B(), A() { }
+    virtual ~D() { }
+};
+
+// we cannot use the if-else pointer method when it comes to const references
+// "When a cast to a reference type fails, the cast throws a std::bad_cast exception" – pg. 826
+//
+// "If bp (t) points (refers) to a Derived (const C&) object, then the cast will ...
+// ...initialize dp (t) to point to the Derived (const C&) object to which bp (t) points"
+
+// creating a template helps to genericise our solution for multiple class types
+template <class T> void f(const T &t) {
+    try {
+        // use C's members
+        const C &c = dynamic_cast<const C&>(t);
+        std::cout << "success: using C's members\n" << std::endl;
+    } catch (std::bad_cast e) {
+        std::cerr << e.what() << ": using A's members\n" << std::endl;
+        // use A's members
+        const A &a = dynamic_cast<const A&>(t);
+    }
+}
+
+int main()
+{
+    A a;
+    std::cout << "Attempting dynamic_cast with \"A a\"\n"; f(a);
+    
+    B b;
+    std::cout << "Attempting dynamic_cast with \"B b\"\n"; f(b);
+    
+    C c;
+    std::cout << "Attempting dynamic_cast with \"C c\"\n"; f(c);
+    
+    D d;
+    std::cout << "Attempting dynamic_cast with \"D d\"\n"; f(d);
+    
+    return 0;
+}
