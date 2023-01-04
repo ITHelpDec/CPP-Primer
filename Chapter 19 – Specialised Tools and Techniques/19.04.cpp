b1ecefb5435f2239558002b22adec4e4f8781577
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2023-01-04 18:34:02 +0000

    Update 19.04.cpp

--- Chapter 19 – Specialised Tools and Techniques/19.04.cpp
+++ Chapter 19 – Specialised Tools and Techniques/19.04.cpp
@@ -4,9 +4,9 @@
  ...rewrite the following code to convert the expression *pa to the type C&:
  
  if (C *pc = dynamic_cast< C* >(pa)) {
- // use C’s members
+     // use C’s members
  } else {
- // use A’s members
+     // use A’s members
  }
  
  >> another not-so-great / vague question
@@ -14,7 +14,7 @@
  >> RTTI doesn't seem to have the best impression (it's frowned on by Google, at least), ...
  >> ...but a better understanding as to why would be more helpful
  
- */
+*/
 
 #include <iostream>
 
@@ -46,7 +46,7 @@ public:
 // "When a cast to a reference type fails, the cast throws a std::bad_cast exception" – pg. 826
 //
 // "If bp (t) points (refers) to a Derived (const C&) object, then the cast will ...
-// ...initialize dp (t) to point to the Derived (const C&) object to which bp (t) points"
+// ...initialize dp (t) to point (refer) to the Derived (const C&) object to which bp (t) points (refers)"
 
 // creating a template helps to genericise our solution for multiple class types
 template <class T> void f(const T &t) {
@@ -54,7 +54,7 @@ template <class T> void f(const T &t) {
         // use C's members
         const C &c = dynamic_cast<const C&>(t);
         std::cout << "success: using C's members\n" << std::endl;
-    } catch (std::bad_cast e) {
+    } catch (const std::bad_cast &e) {
         std::cerr << e.what() << ": using A's members\n" << std::endl;
         // use A's members
         const A &a = dynamic_cast<const A&>(t);
@@ -77,3 +77,19 @@ int main()
     
     return 0;
 }
+
+// output:
+
+// Attempting dynamic_cast with "A a"
+// std::bad_cast: using A's members
+
+// Attempting dynamic_cast with "B b"
+// std::bad_cast: using A's members
+
+// Attempting dynamic_cast with "C c"
+// success: using C's members
+
+// Attempting dynamic_cast with "D d"
+// std::bad_cast: using A's members
+
+// Program ended with exit code: 0
