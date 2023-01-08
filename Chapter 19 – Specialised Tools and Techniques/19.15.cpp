17e6b8f4d9b0004249d7247874e62eb93d3d16f7
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2023-01-08 01:03:11 +0000

    Add files via upload

---
+++ Chapter 19 – Specialised Tools and Techniques/19.15.cpp
@@ -0,0 +1,6 @@
+// Exercise 19.15:
+/*
+ What is the difference between an ordinary function pointer and a pointer to a member function?
+ 
+ >> "Unlike ordinary function pointers, there is no automatic conversion between a member function and a pointer to that member:" – pg. 838
+*/
